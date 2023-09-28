f0b449c23d1e2656a50a4551e6bde66356d0f1fb
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-09-28 17:06:06 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import DrugaKompa from './components/DrugaKompa'
+import PrvaKompa from './components/PrvaKompa'
+import Dohvati from './Dohvati'
+import Login from './components/Login'
+
+function App() {
+  return (
+    <>
+      {/* <PrvaKompa />
+      <DrugaKompa />
+      <Dohvati /> */}
+      <Login />
+    </>
+  )
+}
+
+export default App
